ceab085143f6fd32e8c2f20d94ae6564568146a1
kimdiep <kim.diep.techno@gmail.com>
2019-03-28 15:42:09 +0000

    Wrapped database data in Bookmark instance - refactored tests and methods

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,15 +4,14 @@ require 'pg'
 
 # Bookmark class
 class Bookmark
-
   attr_reader :id, :title, :url
 
   def initialize(id:, title:, url:)
-    @id  = id
+    @id = id
     @title = title
     @url = url
   end
-  
+
   def self.all
     connection = if ENV['ENVIRONMENT'] == 'test'
                    PG.connect(dbname: 'bookmark_manager_test')
@@ -21,7 +20,9 @@ class Bookmark
                  end
 
     result = connection.exec('SELECT * FROM bookmarks;')
-    #we are calling Bookmark.new with the ID, TITLE and URL from the bookmark we just retrieved from the database.
+    # we are calling Bookmark.new with the ID, TITLE and URL from the bookmark we just retrieved from the database.
+    # Here we have wrapped the result object from the database in a Bookmark instance
+    # The Bookmark instance is designed to respond to id and url.
     result.map do |bookmark|
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
     end
@@ -34,6 +35,7 @@ class Bookmark
                    PG.connect(dbname: 'bookmark_manager')
                  end
 
-    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,6 +2,7 @@
 
 require 'pg'
 require 'bookmark'
+require 'database_helpers'
 
 describe Bookmark do
   describe '#all' do
@@ -9,26 +10,30 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
-      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
-      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
-      Bookmark.create(url: "http://www.google.com", title: "Google")
+      bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+      Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+      Bookmark.create(url: 'http://www.google.com', title: 'Google')
 
       bookmarks = Bookmark.all
 
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      #Check bookmark id matches the id of the data persisted in the test database to ensure it is the correct data entry
-      expect(bookmarks.first.id).to eq bookmark.first['id']
+      expect(bookmarks.first.id).to eq bookmark.id
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
+      # Check bookmark id matches the id of the data persisted in the test database to ensure it is the correct data entry
     end
   end
 
   describe '#create' do
     it 'creates a new bookmark' do
-      bookmark = Bookmark.create(url: 'pusheen-test', title: 'Pusheen Title').first
-      expect(bookmark['url']).to eq 'pusheen-test'
-      expect(bookmark['title']).to eq 'Pusheen Title'
+      bookmark = Bookmark.create(url: 'pusheen-test', title: 'Pusheen Title')
+      persisted_data = persisted_data(id: bookmark.id)
+
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.url).to eq 'pusheen-test'
+      expect(bookmark.title).to eq 'Pusheen Title'
     end
   end
 end
